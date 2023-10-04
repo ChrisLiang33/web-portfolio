e6755438f1a345154abbf5034f18a058ec7419d4
liangxianmai <liangxianmai@yahoo.com>
2023-10-04 13:55:26 -0400

    amazing stuff

---
+++ amazing-portfolio/src/components/Footer.svelte
@@ -0,0 +1,30 @@
+<footer class="py-20 sm:py-32 bg:black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justfity-center items-center">
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2">Email</b>chrisliang1@brandeis.edu</p>
+    <p>
+      <b class="pr-2">Github</b>
+      <a href="https://github.com/ChrisLiang33" class="text-violet-400" target="_blank" >
+        Christopher L <sup>
+          <span class="text-xs scale-75 pl-0.5"></span>
+          <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"/>
+        </sup>
+      </a>
+    </p>
+    <p>
+      <b class="pr-2">Linkedin</b>
+      <a href="https://www.linkedin.com/in/xianmai-chris-liang-9b84b5253/" class="text-violet-400" target="_blank" >
+        Christopher L <sup>
+          <span class="text-xs scale-75 pl-0.5"></span>
+          <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"/>
+        </sup>
+      </a>
+    
+    </p>
+
+
+  </div>
+
+</footer>
